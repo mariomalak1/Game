84b81c59f9d303391b51853d741646b679105a35
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-02-21 03:10:31 +0200

    player editation

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,10 @@
 import java.beans.JavaBean;
+import java.util.Scanner;
+
+// this class to hold only single value to pass it with reference in method
+class IntReference{
+    int number;
+}
 
 public class Player {
     protected String name;
@@ -8,9 +14,22 @@ public class Player {
         name = nm;
         symbol = sym;
     }
-    public void get_move(int x, int y){
 
+    // function get move, that has passed to it reference to object that holds integer value only
+    public void get_move(IntReference x, IntReference y){
+        x.number = new Scanner(System.in).nextInt();
+        y.number = new Scanner(System.in).nextInt();
     }
+
+    // getter, setter of name
     public String get_name(){return name;}
+    public void set_name(String name){
+        this.name = name;
+    }
+
+    // getter, setter of symbol
     public char get_symbol(){return symbol;}
+    public void set_symbol(char sy){
+        this.symbol = sy;
+    }
 }
